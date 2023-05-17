c308d88d1e1f326ffadd7163b687d8aacfd4a3af
Fabrizio1919 <fabriziosalerno19@gmail.com>
2023-05-17 17:52:17 +0200

    Style card

--- index.php
+++ index.php
@@ -183,16 +183,18 @@ $devices = [
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Computer Catalog</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
+
 <body>
     <div class="container">
-        <h1>Computer Catalog</h1>
+        <h1>COMPUTER CATALOG</h1>
         <div class="row" id="computer-cards">
-            <?php foreach ($devices as $device): ?>
-                <div class="card">
+            <?php foreach ($devices as $device) : ?>
+                <div class="card shadow m-2 w-50 text-center">
                     <div class="card-body">
                         <h5 class="card-title"><?php echo $device->getType(); ?></h5>
                         <p class="card-text"><?php echo $device->getInfo(); ?></p>
@@ -204,4 +206,5 @@ $devices = [
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
+
 </html>
