d2834e8b8441d06edc0788ca6085657e84e951ce
Fabrizio1919 <fabriziosalerno19@gmail.com>
2023-05-17 17:33:34 +0200

    Fix Date Computer

--- index.php
+++ index.php
@@ -1,42 +1,184 @@
 <?php
 
-// Definizione della classe Computer
-class Computer {
+class Computer
+{
     protected $monitor;
-    protected $motherboard;
+    protected $mbo;
     protected $keyboard;
+    protected $ram;
+    protected $ssd;
+    protected $cpu;
 
-    public function __construct($monitor, $motherboard, $keyboard) {
+    public function __construct($monitor, $mbo, $keyboard, $ram, $ssd, $cpu)
+    {
         $this->monitor = $monitor;
-        $this->motherboard = $motherboard;
+        $this->mbo = $mbo;
         $this->keyboard = $keyboard;
+        $this->ram = $ram;
+        $this->ssd = $ssd;
+        $this->cpu = $cpu;
     }
 
-    public function getInfo() {
-        return "Computer with {$this->monitor}, {$this->motherboard}, and {$this->keyboard}";
+    public function getInfo()
+    {
+        $info = "Monitor: " . $this->monitor->getInfo() . "<br>";
+        $info .= "Motherboard: " . $this->mbo->getInfo() . "<br>";
+        $info .= "Keyboard: " . $this->keyboard->getInfo() . "<br>";
+        $info .= "RAM: " . $this->ram->getInfo() . "<br>";
+        $info .= "SSD: " . $this->ssd->getInfo() . "<br>";
+        $info .= "CPU: " . $this->cpu->getInfo() . "<br>";
+
+        return $info;
+    }
+
+    // Getter e setter per le componenti del computer
+    // ...
+
+}
+
+class Monitor
+{
+    protected $size;
+
+    public function __construct($size)
+    {
+        $this->size = $size;
+    }
+
+    public function getInfo()
+    {
+        return "Size: " . $this->size;
+    }
+
+    // Getter e setter per le proprietà del monitor
+    // ...
+}
+
+class Motherboard
+{
+    protected $model;
+
+    public function __construct($model)
+    {
+        $this->model = $model;
+    }
+
+    public function getInfo()
+    {
+        return "Model: " . $this->model;
+    }
+
+    // Getter e setter per le proprietà della motherboard
+    // ...
+}
+
+class Keyboard
+{
+    protected $type;
+
+    public function __construct($type)
+    {
+        $this->type = $type;
+    }
+
+    public function getInfo()
+    {
+        return "Type: " . $this->type;
+    }
+
+    // Getter e setter per le proprietà della tastiera
+    // ...
+}
+
+class RAM
+{
+    protected $capacity;
+
+    public function __construct($capacity)
+    {
+        $this->capacity = $capacity;
+    }
+
+    public function getInfo()
+    {
+        return "Capacity: " . $this->capacity;
+    }
+
+    // Getter e setter per le proprietà della RAM
+    // ...
+}
+
+class SSD
+{
+    protected $capacity;
+
+    public function __construct($capacity)
+    {
+        $this->capacity = $capacity;
+    }
+
+    public function getInfo()
+    {
+        return "Capacity: " . $this->capacity;
+    }
+
+    // Getter e setter per le proprietà dello SSD
+    // ...
+}
+
+class CPU
+{
+    protected $model;
+
+    public function __construct($model)
+    {
+        $this->model = $model;
+    }
+
+    public function getInfo()
+    {
+        return "Model: " . $this->model;
+    }
+
+    // Getter e setter per le proprietà della CPU
+    // ...
+}
+
+class Desktop extends Computer
+{
+    public function getType()
+    {
+        return "Desktop";
     }
 }
 
-// Definizione della classe Desktop come sottoclasse di Computer
-class Desktop extends Computer {
-    public function getProductType() {
-        return 'Desktop';
+class Laptop extends Computer
+{
+    public function getType()
+    {
+        return "Laptop";
     }
 }
 
-// Definizione della classe Laptop come sottoclasse di Computer
-class Laptop extends Computer {
-    public function getProductType() {
-        return 'Laptop';
-    }
-}
-
-// Creazione dei dati 
-$computerData = [
-    new Desktop('27" LCD', 'Intel Z490', 'Wired Keyboard'),
-    new Laptop('15.6" LED', 'Intel Core i7', 'Backlit Keyboard'),
+// Creazione di un array di oggetti per rappresentare il set di dati
+$devices = [
+    new Desktop(
+        new Monitor("24 inch"),
+        new Motherboard("Model XYZ"),
+        new Keyboard("Mechanical"),
+        new RAM("16GB"),
+        new SSD("500GB"),
+        new CPU("Intel i7")
+    ),
+    new Laptop(
+        new Monitor("15 inch"),
+        new Motherboard("Model ABC"),
+        new Keyboard("Backlit"),
+        new RAM("8GB"),
+        new SSD("256GB"),
+        new CPU("AMD Ryzen 5")
+    ),
 ];
-
 
 ?>
 <!DOCTYPE html>
@@ -49,11 +191,11 @@ $computerData = [
     <div class="container">
         <h1>Computer Catalog</h1>
         <div class="row" id="computer-cards">
-            <?php foreach ($computerData as $computer): ?>
+            <?php foreach ($devices as $device): ?>
                 <div class="card">
                     <div class="card-body">
-                        <h5 class="card-title"><?php echo $computer->getProductType(); ?></h5>
-                        <p class="card-text"><?php echo $computer->getInfo(); ?></p>
+                        <h5 class="card-title"><?php echo $device->getType(); ?></h5>
+                        <p class="card-text"><?php echo $device->getInfo(); ?></p>
                     </div>
                 </div>
             <?php endforeach; ?>
